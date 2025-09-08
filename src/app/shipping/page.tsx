3474728909f1c5b482f7d6c28b0d6e9cf60f94b6
SideeID <sandipratama164026@gmail.com>
2025-09-08 11:06:00 +0700

    feat: create ShippingPage component with Address and Footer sections

---
+++ src/app/shipping/page.tsx
@@ -0,0 +1,11 @@
+import AddressSection from '@/components/sections/AddressSection';
+import FooterNavigationSection from '@/components/sections/FooterNavigationSection';
+
+export default function ShippingPage() {
+  return (
+    <div className='min-h-screen bg-gray-50 mx-auto max-w-[720px] border-x border-gray-200'>
+      <AddressSection />
+      <FooterNavigationSection activeTab='profile' />
+    </div>
+  );
+}
