3de2f58c2cfcff890cbb14cbc50e176b58fdbe1c
SideeID <sandipratama164026@gmail.com>
2025-09-19 17:16:45 +0700

    feat: Update product names and improve styles in SpecialTodaySection for better presentation

--- src/components/sections/shop/SpecialTodaySection.tsx
+++ src/components/sections/shop/SpecialTodaySection.tsx
@@ -9,7 +9,7 @@ import {
 } from '@/components/ui/carousel';
 import Image from 'next/image';
 import { SpecialProduct } from '@/types';
-import { Package, Star, ChevronRight } from 'lucide-react';
+import { Package, Star } from 'lucide-react';
 
 interface SpecialTodaySectionProps {
   title?: string;
@@ -21,56 +21,56 @@ interface SpecialTodaySectionProps {
 const defaultProducts: SpecialProduct[] = [
   {
     id: '1',
-    name: 'Produk 1',
+    name: 'Ova Vprime 60 + Freebase',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '2',
-    name: 'Produk 2',
+    name: 'Ova Vprime 60 + Freebase 2',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '3',
-    name: 'Produk 3',
+    name: 'Ova Vprime 60 + Freebase 3',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '4',
-    name: 'Produk 4',
+    name: 'Ova Vprime 60 + Freebase 4',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '5',
-    name: 'Produk 5',
+    name: 'Ova Vprime 60 + Freebase 5',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '6',
-    name: 'Produk 6',
+    name: 'Ova Vprime 60 + Freebase 6',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '7',
-    name: 'Produk 7',
+    name: 'Ova Vprime 60 + Freebase 7',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
   },
   {
     id: '8',
-    name: 'Produk 8',
+    name: 'Ova Vprime 60 + Freebase 8',
     image: '/assets/SpecialProduct.png',
     originalPrice: 100000,
     discountPrice: 50000,
@@ -94,8 +94,14 @@ export default function SpecialTodaySection({
   return (
     <section className='px-4'>
       <div className='flex items-center justify-between mb-4'>
-        <div className='text-start font-bold text-gray-700'>{title}</div>
-        <ChevronRight className='w-5 h-5 text-gray-600' />
+        <div className='text-start font-bold text-black text-xl'>{title}</div>
+        <Image
+          src='/assets/icon-arrow.png'
+          alt='Arrow'
+          width={20}
+          height={20}
+          className='w-5 h-5'
+        />
       </div>
       <Carousel
         opts={{
@@ -137,10 +143,10 @@ export default function SpecialTodaySection({
                     </div>
                     <p className='text-sm font-semibold'>{product.name}</p>
                     <div className='flex flex-col space-x-1'>
-                      <span className='text-sm font-bold text-red-500'>
+                      <span className='text-lg text-red-400 leading-snug'>
                         {formatPrice(product.discountPrice)}
                       </span>
-                      <span className='text-xs text-gray-500 line-through'>
+                      <span className='text-sm text-gray-500 line-through'>
                         {formatPrice(product.originalPrice)}
                       </span>
                     </div>
