6cd142f4a88f36a93a020b931f2703b1153c6c95
SideeID <sandipratama164026@gmail.com>
2025-11-03 14:34:05 +0700

    feat: update image domains in next.config.ts for improved asset loading

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['icon-library.com', 'console-stg.unionlabs.my.id'],
+    domains: ['console-stg.unionlabs.my.id', 'api.unionlabs.my.id'],
   },
 };
 
